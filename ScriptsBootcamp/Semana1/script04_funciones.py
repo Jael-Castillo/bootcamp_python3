6adceaf7ad99f805309353c466e1f778fe0c9752
Jael-Castillo <jaelcchz@outlook.com>
2019-12-08 21:42:43 -0600

    Tarea4

---
+++ ScriptsBootcamp/Semana1/script04_funciones.py
@@ -0,0 +1,18 @@
+#Tarea3
+#Autor: Jael castillo
+def comparando(n1, n2):
+	if n1 == n2:
+		return'Cuadrado'
+	else:
+		return'Triangulo'
+
+print(comparando(1, 1))
+print(comparando(4, 2))
+print(comparando(3, 3))
+print(comparando(4, 4))
+print(comparando(5, 7))
+print(comparando(9, 7))
+print(comparando(9, 5))
+print(comparando(0, 1))
+print(comparando(6, 6))
+print(comparando(5,12))
